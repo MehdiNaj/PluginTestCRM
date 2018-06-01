0864210ead3637349a3aee3521f386d9b9f35620
MehdiNaj <n.mehdi@outlook.com>
2018-06-01 10:44:06 +0200

    Plugin Crm - version 0

---
+++ PluginsSPKL2/AccountPlugin.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginsSPKL2
+{
+
+    [CrmPluginRegistration(MessageNameEnum.Update, "account", StageEnum.PreOperation, ExecutionModeEnum.Synchronous, "", "PreUpdate Account", 1, IsolationModeEnum.Sandbox)]
+    [CrmPluginRegistration(MessageNameEnum.Create, "account", StageEnum.PreOperation, ExecutionModeEnum.Synchronous, "", "PreCreate Account", 1, IsolationModeEnum.Sandbox)]
+
+    public class AccountPlugin : IPlugin
+    {
+        
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            
+            ITracingService tracer = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IOrganizationServiceFactory factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = factory.CreateOrganizationService(context.UserId);
+
+            Entity entity = (Entity)context.InputParameters["Target"];
+            String entityName = entity.LogicalName;
+            Guid entityId = entity.Id;
+            
+            entity["telephone1"] = "01-234-567";
+            entity["fax"] = "01-234-567";
+            
+            context.InputParameters["Target"] = entity;
+
+
+        }
+    }
+}
---
+++ Solution items/release.bat
@@ -0,0 +1,23 @@
+@echo off
+set connection=%~1
+set package_root=..\..\
+set builduri=%~2
+
+REM Find the spkl in the package folder (irrespective of version)
+For /R %package_root% %%G IN (spkl.exe) do (
+	IF EXIST "%%G" (set spkl_path=%%G
+	goto :continue)
+	)
+
+:continue
+@echo Deploying Using '%spkl_path%' 
+@echo Build URI '%builduri%' 
+
+REM spkl plugins [path] [connection-string] [/p:release]
+"%spkl_path%" plugins "%cd%\Dynamics 365 Demo-Plugin\drop\Build Plugin SPKL\PluginsSPKL2" "%connection%" /p:release
+
+"%spkl_path%" webresources "%cd%\Dynamics 365 Demo-Plugin\drop\Build Plugin SPKL\PluginsSPKL2" "%connection%"
+
+REM %package_root% plugins "%cd%\PluginsSPKL2" "%connection%" /p:release
+
+pause
